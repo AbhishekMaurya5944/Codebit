67b4752fa97fa15ee6fa0b21daa8771f3f3d455f
AbhishekMaurya5944 <mauryabhishek5944@gmail.com>
2024-07-31 02:14:56 +0530

    Changes made

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { v4 as uuidV4 } from 'uuid';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
-
 const Home = () => {
     const navigate = useNavigate();
 
